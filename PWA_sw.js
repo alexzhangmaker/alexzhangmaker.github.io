1701dae73075bb185212b30b96e9f99cb3828341
alexzhangmaker <64229826+alexzhangmaker@users.noreply.github.com>
2023-09-13 13:34:19 +0700

    update PWA

--- PWA_sw.js
+++ PWA_sw.js
@@ -11,6 +11,31 @@ self.addEventListener('activate', () => {
   console.log('service worker 激活成功')
 })
 
+
+
 self.addEventListener('fetch', event => {
   console.log('service worker 抓取请求成功: ' + event.request.url)
+
+  const url = new URL(event.request.url);
+  // If this is an incoming POST request for the
+  // registered "action" URL, respond to it.
+  if (event.request.method === 'POST' && url.pathname === '/bookmark') {
+        event.respondWith((async () => {
+          const formData = await event.request.formData();
+          const link = formData.get('link') || '';
+          const responseUrl = await saveBookmark(link);
+          return Response.redirect(responseUrl, 303);
+        })());
+  }
+});
+
+async function saveBookmark(link){
+  console.log(link) ;
+  return "https://www.google.com/" ;
+}
+
+/*
+self.addEventListener('message', function handler(event: MessageEvent<any>) {
+  console.log(event.data)
 })
+*/
