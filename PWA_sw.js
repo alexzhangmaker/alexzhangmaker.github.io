7d0405857588cadd892b096b5a5e4db7e8711abc
alexzhangmaker <64229826+alexzhangmaker@users.noreply.github.com>
2023-09-26 10:55:29 +0700

    fix

--- PWA_sw.js
+++ PWA_sw.js
@@ -1,32 +1,29 @@
 // sw.js
 console.log('PWA service worker 注册成功')
 
+var cacheName = 'signpostCache';
+var urlsToCache = [
+  'https://alexzhangmaker.github.io/indexPWA.html',
+  'https://alexzhangmaker.github.io/signpostPortfolio.js',
+  'https://alexzhangmaker.github.io/signpostMessage.js',
+  'https://alexzhangmaker.github.io/signpostNews.js',
+  'https://alexzhangmaker.github.io/signpostDividends.js',
+  'https://alexzhangmaker.github.io/signpostCompanyKPI.js',
+  'https://alexzhangmaker.github.io/json/athenaBookmarks.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css',
+  'https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css',
+  'https://alexzhangmaker.github.io/images/signpost.jpeg',
+  'https://alexzhangmaker.github.io/images/poodle.jpeg'
+];
 
 self.addEventListener('install', (event) => {
   // 安装回调的逻辑处理
   //缓存需要的资源，signpost而言就是哪些主要的JSON文件
   let cTime = new Date() ;
   console.log('service worker 安装成功:'+cTime.toLocaleString()) ;
-  /**
-   here to cache data
-   */
-   var cacheName = 'signpostCache';
-   var urlsToCache = [
-    'https://alexzhangmaker.github.io/indexPWA.html',
-    'https://alexzhangmaker.github.io/signpostPortfolio.js',
-    'https://alexzhangmaker.github.io/signpostMessage.js',
-    'https://alexzhangmaker.github.io/signpostNews.js',
-    'https://alexzhangmaker.github.io/signpostDividends.js',
-    'https://alexzhangmaker.github.io/signpostCompanyKPI.js',
-    'https://alexzhangmaker.github.io/json/athenaBookmarks.json',
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
-    'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css',
-    'https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css',
-    'https://alexzhangmaker.github.io/images/signpost.jpeg',
-    'https://alexzhangmaker.github.io/images/poodle.jpeg'
-
-   ];
   
+  /**here to cache data*/
    event.waitUntil(
      caches.open(cacheName)
      .then(async function(cache){
@@ -49,6 +46,7 @@ self.addEventListener('activate', function(event) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
           if (cacheAllowlist.indexOf(cacheName) === -1) {
+            console.log('activate caches.delete:'+cacheName) ;
             return caches.delete(cacheName);
           }
         })
@@ -65,10 +63,12 @@ self.addEventListener('fetch', event => {
     caches.match(event.request).then(function(response) {
         // 缓存中有对应的资源，直接返回
         if (response) {
+          console.log('缓存中有对应的资源，直接返回') ;
           return response;
         }
 
         // 缓存中没有对应的资源，从网络获取
+        console.log('缓存中没有对应的资源，从网络获取') ;
         return fetch(event.request);
       })
   );
